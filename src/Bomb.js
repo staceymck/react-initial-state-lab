12f1181b416a67a29d4d10a361cf047844f60626
Stacey McKnight <mcknight.stm@gmail.com>
2021-04-12 15:40:55 -0500

    commit tree ac86031d3230591e79c7663915265a3662ef6bf5

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,13 @@
-// your Bomb code here!
+import React from "react";
+
+export default class Bomb extends React.Component {
+  constructor(props) {
+    super()
+    this.state = {
+      secondsLeft: props.initialCount
+    }
+  }
+  render() {
+    return (this.state.secondsLeft === 0 ? <p>Boom!</p> : <p>{this.state.secondsLeft} seconds left before I go boom!</p>)
+  }
+}
